e05a5cb33b07cd76cc6c2fab41c9635d60508ad3
Dulatto <dulat.omarov@gmail.com>
2022-06-21 22:36:20 -0400

    authpage

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -32,6 +32,34 @@ function AuthPage() {
                             <a href="/" className="btn-outline btn-reg">Don't have an account?</a>
                         </div>
                     </form>
+
+                    <h3>Registration</h3>
+                    <form className="form form-login">
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    className='validate'
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className='validate'
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <button className="wawes-effect wawes-light btn blue">
+                                Registration
+                            </button>
+                            <a href="/" className="btn-outline btn-reg">Already have an account?</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </React.Fragment>
