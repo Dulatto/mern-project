303412d396334cf727470af3c5354f9448f79db5
Dulatto <dulat.omarov@gmail.com>
2022-07-11 22:43:11 -0400

    react router dom

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,68 +1,76 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function AuthPage() {
     return (
-        <React.Fragment>
-            <div className="container">
-                <div className="auth-page">
-                    <h3>Autorization</h3>
-                    <form className="form form-login">
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <input
-                                    type="email"
-                                    name="email"
-                                    className='validate'
-                                />
-                                <label htmlFor="email">Email</label>
-                            </div>
-                            <div className="input-field col s12">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    className='validate'
-                                />
-                                <label htmlFor="password">Password</label>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <button className="wawes-effect wawes-light btn btn blue">
-                                Submit
-                            </button>
-                            <a href="/" className="btn-outline btn-reg">Don't have an account?</a>
-                        </div>
-                    </form>
+        <BrowserRouter>
+            <Routes>
+                <React.Fragment>
+                    <div className="container">
+                        <div className="auth-page">
+                            <Route path='/login'>
+                                <h3>Autorization</h3>
+                                <form className="form form-login">
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                className='validate'
+                                            />
+                                            <label htmlFor="email">Email</label>
+                                        </div>
+                                        <div className="input-field col s12">
+                                            <input
+                                                type="password"
+                                                name="password"
+                                                className='validate'
+                                            />
+                                            <label htmlFor="password">Password</label>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <button className="wawes-effect wawes-light btn btn blue">
+                                            Submit
+                                        </button>
+                                        <a href="/" className="btn-outline btn-reg">Don't have an account?</a>
+                                    </div>
+                                </form>
+                            </Route>
 
-                    <h3>Registration</h3>
-                    <form className="form form-login">
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <input
-                                    type="email"
-                                    name="email"
-                                    className='validate'
-                                />
-                                <label htmlFor="email">Email</label>
-                            </div>
-                            <div className="input-field col s12">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    className='validate'
-                                />
-                                <label htmlFor="password">Password</label>
-                            </div>
+                            <h3>Registration</h3>
+                            <form className="form form-login">
+                                <div className="row">
+                                    <div className="input-field col s12">
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            className='validate'
+                                        />
+                                        <label htmlFor="email">Email</label>
+                                    </div>
+                                    <div className="input-field col s12">
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            className='validate'
+                                        />
+                                        <label htmlFor="password">Password</label>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <button className="wawes-effect wawes-light btn blue">
+                                        Registration
+                                    </button>
+                                    <a href="/" className="btn-outline btn-reg">Already have an account?</a>
+                                </div>
+                            </form>
                         </div>
-                        <div className="row">
-                            <button className="wawes-effect wawes-light btn blue">
-                                Registration
-                            </button>
-                            <a href="/" className="btn-outline btn-reg">Already have an account?</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </React.Fragment>
+                    </div>
+                </React.Fragment>
+            </Routes>
+
+        </BrowserRouter>
     )
 }
 
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -10,6 +10,13 @@ router.post('/registration', async (req, res) => {
         if (isUsed) {
             return res.status(300).json({ message: 'This email is already used, try again' })
         }
+
+        const user = new User(email, password)
+
+        await user.save()
+
+        res.status(201).json({ message: 'User was created' })
+
     } catch (error) {
         console.log(error)
     }
